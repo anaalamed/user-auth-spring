707908fc40087b23259db5cb1adea74149660c67
Ana Levit <anaalamed@gmail.com>
2022-11-08 19:24:55 +0200

    validate class

--- src/main/java/auth_spring/controllers/AuthController.java
+++ src/main/java/auth_spring/controllers/AuthController.java
@@ -6,6 +6,7 @@ import auth_spring.controllers.responses.TokenResponse;
 import auth_spring.model.User;
 import auth_spring.services.AuthService;
 import auth_spring.services.UserService;
+import auth_spring.utils.Validate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,14 +27,22 @@ public class AuthController {
 
     @RequestMapping(method = RequestMethod.POST, path = "/signup")
     public ResponseEntity<User> addUser(@RequestBody UserRequest userRequest){
-//        boolean isValidateUser = validateUser(email, name, password);
+        boolean isValidateUser = Validate.validateUserFields(userRequest.getEmail(), userRequest.getName(), userRequest.getPassword());
+
+        if (!isValidateUser) {
+            throw new IllegalArgumentException("input is not valid, registration failed");
+        }
 
         return ResponseEntity.ok(authService.add(userRequest));
     }
 
     @RequestMapping(method = RequestMethod.POST, path = "/login")
     public ResponseEntity login(@RequestBody UserRequest userRequest){
-//        boolean isValidateUser = validateUser(email, password);
+        boolean isValidateUser = Validate.validateUserFields(userRequest.getEmail(), userRequest.getPassword());
+
+        if (!isValidateUser) {
+            throw new IllegalArgumentException("input is not valid, login failed");
+        }
 
         return ResponseEntity.ok(new TokenResponse(authService.login(userRequest)));
     }
